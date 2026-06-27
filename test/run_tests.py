fd0d3fdbae3c6bd83b3489ccd165b606277eaa13
Nikola D. Georgiev <nikola.georgiev@sap.com>
2026-06-27 18:11:58 +0300

    tests(3)

---
+++ test/run_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+import pytest
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([
+        "unit/loginTest.py",
+        "unit/registrationTest.py",
+        "integration/smokeIT.py",
+        "-v",
+    ]))
--- test/unit/loginTest.py
+++ test/unit/loginTest.py
@@ -28,6 +28,20 @@ class LoginTest(unittest.TestCase):
 
         delete(user_id)
 
+    def test_login_when_logged_returns_200(self):
+        email = f"login.already.{int(time.time())}@example.com"
+        password = "hunter"
+
+        _, body = register(email, password)
+        user_id = body["id"]
+
+        login(email, password)
+        status, body = login(email, password)
+        self.assertEqual(status, 200)
+        self.assertTrue(body.get("logged_in"))
+
+        delete(user_id)
+
     def test_login_with_invalid_credentials_returns_401(self):
         status, body = login(f"nonexistent.{int(time.time())}@example.com", "hunter")
         self.assertEqual(status, 401)
